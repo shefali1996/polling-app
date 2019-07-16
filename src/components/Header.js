7b49d0929cab32ca0d76b940a2c6d3a6de81a46a
shefali1996 <shefali@xcellencetechnologies.info>
2019-07-16 12:03:08 +0530

    Corrected the refreshing of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import {Link} from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
@@ -8,8 +9,8 @@ export default class Header extends Component {
         <Navbar bg="primary" variant="dark">
           <Navbar.Brand>Navbar</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="/">Login</Nav.Link>
-            <Nav.Link href="/register">Sign Up</Nav.Link>
+            <Link to='/'>Login</Link>
+            <Link to='/register'>Register</Link>
           </Nav>
         </Navbar>
       </div>
